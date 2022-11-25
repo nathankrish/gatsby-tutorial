7e98f980bbae7551cff541b1f19e3b436f362f1c
Krish Nathan <nathan.krish.edu@gmail.com>
2022-11-24 22:27:49 -0500

    About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const AboutPage: React.FC = () => 
+    <main>
+        <h1>About Me</h1>
+        <p>Hi there! I'm Krish</p>
+    </main>
+;
+
+export const Head: React.FC = () => <title>About Me</title>
+
+export default AboutPage;
